1efe004f6e86fee7c86d031e004c869ee52c7e53
Irina Prokopova <irina-prokopova.style@yandex.ru>
2024-10-17 12:30:54 +0300

    add migrations and registrate models

--- catalog/views.py
+++ catalog/views.py
@@ -3,13 +3,15 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
 
 
 def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        return HttpResponse(f'Спасибо, {name}, за Ваше сообщение! Наши специалисты скоро свяжутся с Вами по номеру телефона {phone}!')
-    return render(request, 'contacts.html')
+    if request.method == "POST":
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
+        return HttpResponse(
+            f"Спасибо, {name}, за Ваше сообщение! Наши специалисты скоро свяжутся с Вами по номеру телефона {phone}!"
+        )
+    return render(request, "contacts.html")
