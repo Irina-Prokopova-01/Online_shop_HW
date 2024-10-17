b5de60f2802bca4fc01f0eedc038f1c075f11799
Irina Prokopova <irina-prokopova.style@yandex.ru>
2024-10-17 20:53:50 +0300

    correct new

--- catalog/management/commands/add_products.py
+++ catalog/management/commands/add_products.py
@@ -1,22 +1,23 @@
 from django.core.management.base import BaseCommand
 from catalog.models import Category, Product
 
+
 class Command(BaseCommand):
-    help = 'test'
+    help = 'Add test products to the database'
 
     def handle(self, *args, **kwargs):
         Category.objects.all().delete()
         Product.objects.all().delete()
 
-        category, _ = Category.objects.get_or_create(title='птица')
+        category, _ = Category.objects.get_or_create(title='птица', description='просто птица')
 
         products = [
             {'title': 'бедро куринное', 'description': 'Свежее', 'category': category, 'price': '400', 'created_at': '2024-12-12', 'updated_at': '2024-12-10'},
             {'title': 'голени куринные', 'description': 'Свежее', 'category': category, 'price': '300', 'created_at': '2024-10-10', 'updated_at': '2024-12-11'},
         ]
 
-        for product_data in products:
-            product, created = Product.objects.get_or_create(**product_data)
+        for product in products:
+            product, created = Product.objects.get_or_create(**product)
             if created:
                 self.stdout.write(self.style.SUCCESS(f'Successfully added product: {product.title}'))
             else:
