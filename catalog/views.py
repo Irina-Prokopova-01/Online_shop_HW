e040b66e80451c9432e50a325b6b40ef8f7cdfb5
Irina Prokopova <irina-prokopova.style@yandex.ru>
2024-10-24 14:40:39 +0300

    add home info in views

--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 from .models import Product
 
 
-def home(request):
-    return render(request, "home.html")
+def home(request: HttpRequest):
+    """функция обрабатывает запрос и возвращает html-страницу"""
+    if request.method == "GET":
+        products = Product.objects.all()
+        context = {"products": products}
+
+        return render(request, "home.html", context=context)
 
 
 def contacts(request):
+    """Обрабатываем форму и возвращаем ответ"""
     if request.method == "POST":
         name = request.POST.get("name")
         phone = request.POST.get("phone")
